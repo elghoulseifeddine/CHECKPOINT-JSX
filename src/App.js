653636ea8c254e5efad4f9c60158274c687c1556
Abderrahim Trabelsi <trabelsi.abderrahim@petrogas.local>
2021-10-20 11:34:39 +0100

    Checkpoint JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import imgtwo from './imgtwo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all">
+    
+         <div className= "position" style={{border:"solid 1px black", maxWidth:"100vw"}}>
+
+<h1 className="title red">WIIW-MAN</h1>
+
+<br/>
+
+<img src={imgtwo} alt="img"/>
+
+<br/>
+
+<img src="./imgone.jpg" alt="img"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="https://www.youtube.com/watch?v=uNfzsrrLrsY&ab_channel=DALYTALIANI" type="video/mp4"/>
+
+</video>
+
     </div>
+    
   );
 }
 
